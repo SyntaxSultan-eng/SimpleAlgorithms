9ab0f0ee5d3067bae42c6790192575f32788f923
Vlad <vladislav5196@mail.ru>
2025-01-03 18:58:56 +0300

    Update Single linked list. New methods

--- Structures/Singlelink.py
+++ Structures/Singlelink.py
@@ -12,7 +12,56 @@ class SingleLinkedList:
             self.value = value
             self.next = next
 
-    def append(self, value):
+    def lenght(self) -> int:
+        '''
+        Output the length of a singly linked list
+        '''
+        if self.head is None:
+            return 0
+        
+        counter = 1 #Counted the head element
+        current = self.head
+
+        while current.next: #We go through the elements of the array and get to the last one
+            current = current.next
+            counter += 1
+        return counter
+    
+    def get(self, index: int) -> int:
+        '''
+        Outputs the element by index
+        '''
+        if index < 0 or index >= self.lenght():
+            return -1
+        if index == 0:
+            return self.head.value
+        
+        current = self.head
+        counter = 1 #Counted the head element
+        
+        while current.next:
+            if counter == index:
+                return current.next.value # Get the element
+            current = current.next
+            counter += 1
+    
+    def addAtHead(self, val: int) -> None:
+        '''
+        Adding an element to the head
+        '''
+        if self.head is None:
+            self.head = self.Node(val) #Checking if our list is empty
+            return
+        
+        current = self.Node(val) #Making a new element - head
+        current.next = self.head 
+        self.head = current
+        return
+
+    def append(self, value: int) -> None:
+        '''
+        Adding an element to the tail
+        '''
         if self.head is None:
             self.head = self.Node(value) #Checking if our list is empty
             return 
@@ -24,7 +73,59 @@ class SingleLinkedList:
         current.next = self.Node(value) #Adding an element to the array
         return
     
-    def out(self):
+    def addAtIndex(self, index: int, val: int) -> None:
+        '''
+        Adding an element by index
+        '''
+        if index < 0 or index > self.lenght():
+            return -1
+        if index == 0:
+            if self.head is None:
+                self.head = self.Node(val) # Create linked list
+                return
+            self.addAtHead(val) #Adding an element to the head
+            return
+        counter = 1
+        current = self.head
+    
+        while current.next:
+            if counter == index:
+                last_current = current.next
+                current.next = self.Node(val) #Adding an element
+                current.next.next = last_current
+                return
+            counter += 1
+            current = current.next
+        
+        # If the index points to the last element
+        last_current = current.next
+        current.next = self.Node(val) 
+        current.next.next = last_current
+
+    def deleteAtIndex(self, index: int) -> None:
+        '''
+        Deleting an element by index
+        '''
+        if index < 0 or index >= self.lenght():
+            return
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        counter = 1
+        current = self.head
+    
+        while current.next:
+            if counter == index:
+                current.next = current.next.next #Just changing the binding of the elements
+                return
+            counter += 1
+            current = current.next
+        
+    def out(self) -> None:
+        '''
+        Output this array beautifully as a list
+        '''
         if self.head is None:
             print("List is empty") # Checking the length of our array
             return
@@ -37,11 +138,22 @@ class SingleLinkedList:
 
         print(str(current.value) + "]")
         return
+    
+###########################################
 
 # This is simple TEST
 
 list = SingleLinkedList()
-list.append(15)
-list.append(19)
-list.append(-17)
+list.addAtHead(7)
+list.addAtHead(2)
+list.addAtHead(1)
+list.addAtIndex(3,0)
+list.deleteAtIndex(2)
+list.addAtHead(4)
+list.append(4)
+list.out()
+print(list.get(4))
+list.addAtHead(4)
+list.addAtIndex(5,0)
+list.addAtHead(6)
 list.out()
